c7ebea6da5f80c7c5b90110e7328e7fa03dd4324
Michael Harmer <michaeltharmer@gmail.com>
2023-09-15 22:27:12 -0400

    Configured react with rails

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,9 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 import App from "./app"
+import React from "react"
+import { createRoot } from "react-dom/client"
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);
